e90e506dd4d7ba0e17e8989b294006556e5696e0
9sen9-9 <sen9_69@mail.ru>
2024-09-23 02:52:48 +0300

    DZ

--- src/Main.java
+++ src/Main.java
@@ -2,57 +2,62 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        byte b = 67;
+        short s = -159;
+        int i = 27897;
+        long L = 987678965549L;
+        double d = 27.12;
+        float f = 2.786F;
+        byte teacher1 = 23;
+        byte teacher2 = 27;
+        byte teacher3 = 30;
+        short list = 480;
+        int total = teacher1 + teacher2 + teacher3;
+        int total2 = list / total;
+        System.out.println( "На каждого ученика рассчитано" + total2 + "листов бумаги");
+ byte bottle = 16;
+ byte time = 2;
+ int min = bottle * 20;
+        System.out.println("За 20 минут машина произвела"+min);
+        int day = bottle * 1440;
+        System.out.println("За сутки машина произвела" + day);
+        int days = bottle * 4320;
+        System.out.println("За 3 суток машина произвела"+ days);
+        int month = bottle * 43200;
+        System.out.println("За месяц машина произвела"+ month);
+        int banana = 5 * 80;
+        int milk = 105 * 2;
+        int iceCream = 100 * 2;
+        int egg =70 * 4;
+        int all = banana + milk + iceCream + egg;
+        System.out.println(all);
+        int throwOff = 7000;
+        int gram1 = 250;
+        int gram2 = 500;
+        int mass1 = throwOff / gram1;
+        int mass2 = throwOff / gram2;
+        int kg =(mass1 + mass2) / 2;
+        System.out.println(kg);
+      int masha = 67760;
+      int denis = 83690;
+      int cris = 76230;
+      int masha1 = masha / 100 * 10;
+        System.out.println(masha1);
+      int denis1 = denis / 100 * 10;
+        System.out.println(denis1);
+       int cris1 = cris / 100 * 10;
+        System.out.println(cris1);
+      int masha2 = masha1 + masha;
+      int denis2 = denis1 + denis;
+      int cris2 = cris1 + cris;
+     int masha3 = masha1 * 12;
+     int denis3 = denis1 * 12;
+     int cris3 = cris * 12;
+        System.out.println("Маша теперь получает" + masha2+ "доход вырос на" + masha3);
+        System.out.println("Маша теперь получает" + denis2+ "доход вырос на" + denis3);
+        System.out.println("Маша теперь получает" + cris2 + "доход вырос на" + cris3);
 
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        dog = dog - 3.5;
-        cat = cat - 1.6;
-        paper = paper - 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        var friend = 19;
-        System.out.println(friend);
-        friend = friend * 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
-        var boxer1 = 82.7;
-        var boxer2 = 78.2;
-        var common = boxer1 % boxer2;
-        System.out.println(common);
-        var AllTime = 640;
-        var employee = 8;
-        var total = AllTime / employee;
-        System.out.println(total);
-        var x = 94;
-        x = x + total;
-        System.out.println(x);
-        x = x % employee;
-        System.out.println(x);
 
 
 
-}
-
-}
-
-
+    }}
