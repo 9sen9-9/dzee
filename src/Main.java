9671bff0483ea9e48c91ff4c37cfecfddd304245
9sen9-9 <sen9_69@mail.ru>
2024-10-02 00:51:13 +0300

    DZ

--- src/Main.java
+++ src/Main.java
@@ -2,81 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int lipton = 400000;
-        System.out.println(lipton);
-        byte phone = 60;
-        System.out.println(phone);
-        short cs = 10000;
-        System.out.println(cs);
-        long dota = 100000000L;
-        System.out.println(dota);
-        float milka = 1.3f;
-        System.out.println(milka);
-        double creatine = 10.30;
-        System.out.println(creatine);
-        byte b = 67;
-        short s = -159;
-        int i = 27897;
-        long L = 987678965549L;
-        double d = 27.12;
-        float f = 2.786F;
-        int teacher1 = 23;
-        int teacher2 = 27;
-        int teacher3 = 30;
-        int list = 480;
-        int total = teacher1 + teacher2 + teacher3;
-        int total2 = list / total;
-        System.out.println( "На каждого ученика рассчитано" + total2 + "листов бумаги");
- byte bottle = 16;
- byte time = 2;
- int min = (bottle * 20)/2;
-        System.out.println("За 20 минут машина произвела"+min);
-        int day = (bottle * 1440)/2;
-        System.out.println("За сутки машина произвела" + day);
-        int days = (bottle * 4320)/2;
-        System.out.println("За 3 суток машина произвела"+ days);
-        int month = (bottle * 43200)/2;
-        System.out.println("За месяц машина произвела"+ month);
-        int total1 = 120;
-        int white = 2;
-        int brown = 4;
-        int all = 120 / (white+brown);
-        int totalWhite = all * white;
-        int totalBrown = all * brown;
-        System.out.println("В школе, где" + all + "классов, нужно" + totalWhite + "банок белой краски и" + totalBrown + "банок коричневой краски");
-        int banana = 5 * 80;
-        int milk = 105 * 2;
-        int iceCream = 100 * 2;
-        int egg =70 * 4;
-        int all1 = banana + milk + iceCream + egg;
-        System.out.println(all);
-        int throwOff = 7000;
-        int gram1 = 250;
-        int gram2 = 500;
-        int mass1 = throwOff / gram1;
-        int mass2 = throwOff / gram2;
-        int kg =(mass1 + mass2) / 2;
-        System.out.println(kg);
-      int masha = 67760;
-      int denis = 83690;
-      int cris = 76230;
-      int masha1 = masha / 100 * 10;
-        System.out.println(masha1);
-      int denis1 = denis / 100 * 10;
-        System.out.println(denis1);
-       int cris1 = cris / 100 * 10;
-        System.out.println(cris1);
-      int masha2 = masha1 + masha;
-      int denis2 = denis1 + denis;
-      int cris2 = cris1 + cris;
-     int masha3 = masha1 * 12;
-     int denis3 = denis1 * 12;
-     int cris3 = cris * 12;
-        System.out.println("Маша теперь получает" + masha2+ "доход вырос на" + masha3);
-        System.out.println("Маша теперь получает" + denis2+ "доход вырос на" + denis3);
-        System.out.println("Маша теперь получает" + cris2 + "доход вырос на" + cris3);
+ int age = 18;
+ if (age => 18 );  }
 
 
 
-
-    }}
+    }
