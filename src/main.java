76f84758631589ee298ba573d7f65f01fe851735
9sen9-9 <sen9_69@mail.ru>
2024-10-21 22:04:53 +0300

    DZ

--- src/main.java
+++ src/main.java
@@ -1,103 +1,51 @@
 import javax.swing.*;
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class main {
     public static void main(String[] args) {
-///1
-        System.out.println("Task1");
-        int delay = 15000;
-        int total = 0;
-        int i = 0;
-        for (; total < 2_459_000; i++) {
-            total = delay + total + (total / 100);
-            System.out.println("Месяц" + i + "сумма накоплений равна" + total + "рублей");
+///1.2
+        System.out.println("Task1,2");
+        int[] numbers = new int[]{1, 2, 3};
+        numbers[0] = 1;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + ",");
         }
-        ///2
-        System.out.println("Task2");
-        int i1 = 0;
-        while (i1 < 10) {
-            i1 = i1 + 1;
-            System.out.print(i1);
+        System.out.println();
+        double[] arr = {1.57, 7.654, 9.986};
+        arr[0] = 1.57;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            System.out.print(",");
         }
-        int b = 10;
-        for (; b > 1; ) {
-            b = b - 1;
-            System.out.print(b);
-        }
+        System.out.println();
 
 
-        ///3
-        System.out.println("task3");
-        int Y = 12000000;
-        for (i = 1; i < 10; i++) {
-            int birthRate = 17 * Y / 1000;
-            int dead = 8 * Y / 1000;
-            Y = Y + birthRate - dead;
-            System.out.println("Год" + i + "численность населения составляет" + Y);
-        }
-///4
-        System.out.println("Task4");
-        int contribution = (15000 * 7) / 100;
-        int percent = contribution + 15000;
-        int month = 1;
-        contribution = contribution + percent;
-        while (i <= 12000000) {
-            month++;
-            i = i + contribution;
-            System.out.println("месяц" + month + "сумма накопления" + i + "рублей");
-        }
-        ///5
-        System.out.println("Task5");
-        int contribution1 = (15000 * 7) / 100;
-        int percent1 = contribution1 + 15000;
-        int month1 = 1;
-        contribution = contribution + percent;
-        while (i < 12000000) {
-            month1++;
-            i = percent1 + contribution1;
-            if (month1 % 6 == 0) ;
-            System.out.println("месяц" + month1 + "сумма накопления" + i + "рублей");
+        {
 
-            {
+            System.out.println("Task3");
+            int[] numbers1 = {1, 2, 3};
+            numbers[0] = 1;
+            for (int i1 = 0; i1 >= numbers.length; i1--) {
+                System.out.println(numbers[i1] + ",");
+            }
 
-
+            double[] arr1 = {1.57, 7.654, 9.986};
+            arr1[0] = 1.57;
+            for (int i1 = 0; i1 >= arr1.length; i1--) {
+                System.out.print(arr1[i1] + ",");
 
             }
-        }
-        ///6
-        System.out.println("Task6");
-        int deposit = 15000;
-        int month2 = 0;
-        while (month2 < 108) {
-            month2 = month2+6;
-            deposit = deposit + deposit * 7 / 100;
-            if (month2 % 6 == 0) {
+            System.out.println("Task4");
+            int[] code = {1, 2, 3};
+            for (int i = 0; i < code.length; i++) {
+                if (code[i] % 2 != 0) {
+                    code[i] = code[i] + 1;
+                }
             }
-            System.out.println("сумма накоплений за" + month2 + "месяц равна" + deposit + "рублей");
+            System.out.print(Arrays.toString(code));
 
 
-        }
-        ///7
-        System.out.println("Task7");
-        for (int i2 = 1; i2 < 31; i2=i2+6) {
-            if (i2 % 7 == 0) {
-            }
-            System.out.println("сегодня пятница"+i2+"-е число,необходимо подготовить отчет");
+        }}}
 
-
-        }
-
-        ///8
-        System.out.println("Task8");
-        int juniorYear = 1823;
-        int finalYear = 2123;
-        int realYear = 2023;
-        for (i=0;i<finalYear;i = i + 79)
-            if (i>juniorYear){
-                System.out.println(i);
-            }
-    }
-
-
-}
