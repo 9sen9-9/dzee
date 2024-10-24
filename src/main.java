2b99160a476f13d341edeb19fc6d2d6d48333e1e
9sen9-9 <sen9_69@mail.ru>
2024-10-24 18:18:01 +0300

    DZ

--- src/main.java
+++ src/main.java
@@ -16,36 +16,40 @@ public class main {
         double[] arr = {1.57, 7.654, 9.986};
         arr[0] = 1.57;
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-            System.out.print(",");
+            System.out.print(arr[i] + ",");
+
+
+
         }
-        System.out.println();
-
-
-        {
-
             System.out.println("Task3");
-            int[] numbers1 = {1, 2, 3};
-            numbers[0] = 1;
-            for (int i1 = 0; i1 >= numbers.length; i1--) {
-                System.out.println(numbers[i1] + ",");
+
+        int[] numbers1 = new int[]{1, 2, 3};
+        numbers1[0] = 1;
+        for (int i = numbers1.length - 1; i >= 0; i--) {
+            System.out.print(numbers1[i]+",");
+
+
+        }
+
+        double[] arr1 = {1.57, 7.654, 9.986};
+        arr1[0] = 1.57;
+        for (int i = arr1.length - 1; i >= 0; i--) {
+            System.out.print(arr1[i]+",");
+
+
             }
 
-            double[] arr1 = {1.57, 7.654, 9.986};
-            arr1[0] = 1.57;
-            for (int i1 = 0; i1 >= arr1.length; i1--) {
-                System.out.print(arr1[i1] + ",");
 
+
+        System.out.println("Task4");
+        int[] code = {1, 2, 3};
+        for (int i = 0; i < code.length; i++) {
+            if (code[i] % 2 != 0) {
+                code[i] = code[i] + 1;
             }
-            System.out.println("Task4");
-            int[] code = {1, 2, 3};
-            for (int i = 0; i < code.length; i++) {
-                if (code[i] % 2 != 0) {
-                    code[i] = code[i] + 1;
-                }
-            }
-            System.out.print(Arrays.toString(code));
+        }
+        System.out.print(Arrays.toString(code));
 
 
-        }}}
-
+    }
+}
